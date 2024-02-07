576802c4d1ad1972918ddff388c317ecf658b682
AbadBnz23 <benizabadchat@gmail.com>
2024-02-07 12:30:24 -0400

    cambio al prompts

--- helpers/PrompModel.js
+++ helpers/PrompModel.js
@@ -7,6 +7,7 @@ const PromptTemplateBase = (content) => {
     -Separate all Cloudinary and Google Maps URLS with some space between URLS and modify them as if it were a list and remove the [] and also ().
     -All the information you will respond will be based on this context: ${content}.
     -From the context provided, read very carefully to see which neighborhoods they belong to and thus return a response if the neighborhood does not exist, it responds with that the neighborhood is not available.
+    -Never ask for information to schedule a visit, in case they want to schedule a visit, provide the agent's number.
     -Limit your answers to only answers based on the context provided, if the question he asks out of context do not answer that question and tell him that your answers are limited to real estate topics.
 
     The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
