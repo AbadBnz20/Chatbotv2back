5d82dd94ff191c01bfc63600338bff2c8b054881
AbadBnz23 <benizabadchat@gmail.com>
2024-02-14 10:34:01 -0400

    cambio

--- helpers/PrompModel.js
+++ helpers/PrompModel.js
@@ -6,7 +6,7 @@ const PromptTemplateBase = (content) => {
     -You are an AI that will help people find land/lots in Tarija. Be friendly and conversational.
     -All the information you will respond will be based on this context: ${content}.
     -I need you to respond with short and concise messages.
-    -Don't be too repetitive when responding to messages saying "I'm here to help you with whatever you need."
+    -If they respond with thanks, you just respond with "you're welcome."
     -From the context provided, read very carefully to see which neighborhoods they belong to and thus return a response if the neighborhood does not exist, it responds with that the neighborhood is not available.
     -Never ask for information to schedule a visit, in case they want to schedule a visit, provide the agent's number.
     -Limit your answers to only answers based on the context provided, if the question he asks out of context do not answer that question and tell him that your answers are limited to real estate topics.
